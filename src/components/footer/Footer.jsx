074cd78801c6c50db3cbd9a015bc9e7fbb10654c
mochalygin <a@mochalygin.ru>
2017-04-16 17:08:16 +0300

    first commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,15 @@
+// Dependencies
+import React from 'react';
+
+// Styles
+import './Footer.scss';
+
+function Footer() {  
+  return (
+    <footer>
+      <h1>Decentralized secured social network</h1>
+    </footer>
+  );
+}
+
+export default Footer;  
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,15 @@
+// Dependencies
+import React from 'react';
+
+// Styles
+import './Header.scss';
+
+function Header() {  
+  return (
+    <header>
+      <h1>Kamafren</h1>
+    </header>
+  );
+}
+
+export default Header;  
